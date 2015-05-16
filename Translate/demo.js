02dfde92e68bac30e9e2dd97d7045e3f3a70bcac
lcrescenzo <luis.crescenzo@gmail.com>
2015-05-16 15:39:08 -0300

    repeater+translate V0.0.0.1

---
+++ Translate/demo.js
@@ -0,0 +1,32 @@
+function Demo(jQuery) {
+	var multi = '{ "pt-br" : { \r\n language : "Potuguês do Brasil"}, \r\n "en-us" : { \r\n language : "English of USA"} }';
+	var resources = '{ language : "Potuguês do Brasil" }';
+	var Load = function (){
+		var selectType = jQuery("#sltType");
+		var txtResource =  jQuery("#txtResource")
+		var btnLoadResource = jQuery("#btnLoadResource");
+		
+		selectType.change(function(){
+			txtResource.val(jQuery(this).val() === "resources" ? resources : multi); 
+		});
+		
+		
+		btnLoadResource.click(function(){
+			var data = JSON.parse(txtResource.val());
+			
+			jQuery.translate({resources : data});
+			
+			var label = jQuery("#lbl").clone(true);
+			
+			var lblPlace =jQuery("#lblPlace");
+			lblPlace.html(label);
+			lblPlace.translate();
+		});
+		
+		
+		
+		
+	};
+	
+	Load();
+}
---
+++ Translate/jquery.translate.js
@@ -0,0 +1,54 @@
+(function ($) {
+    
+    var tag = "translate";
+    var localeDefault = "pt-br";
+	var settings = {};
+	
+    function getResource(locale){
+        //TODO: tratar quando nao existir  elemento
+        return settings.resources[locale] || settings.resources;
+    }
+    
+	function getMessage(key, locale){
+		return LoadDataByNamespace(key.split("."), getResource(locale));
+	}
+	
+	
+	function LoadDataByNamespace(namespaces, data, index){
+		index = index || 0;
+        
+        if(namespaces.length >= index)
+            return data;
+        
+        if(data === null)
+            return null;;
+            
+        LoadDataByNamespace(namespaces, data, index++);
+	}
+	
+    $.fn.translate = function (options, key, locale) {
+		locale = locale || localeDefault;
+		
+		if (options === "getMassage") {
+			return getMessage(key, locale);
+		}
+        // Establish our default settings
+        settings = $.extend({
+            resources : { "pt-br": null }
+        }, options);
+
+        return this.each(function () {
+            var children = $(this).find("[" + tag + "]");
+            if (children.length > 0){
+                children.forEach((element) => {
+                    var translation = LoadDataByNamespace(element.attr(tag), getResource(locale));
+                    if(translation !== null){
+                        element.html(translation);
+                    }
+                    element.removeAttr(tag);
+                });
+                
+            }
+        });
+    };
+})(jQuery);
